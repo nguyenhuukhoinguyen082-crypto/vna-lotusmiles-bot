2d29b5d3bdd8e2d6e35cfafa897c006a6e6ebbe5
nguyenhuukhoinguyen082-crypto <nguyenhuukhoinguyen082@gmail.com>
2026-09-08 15:41:05 +0700

    Fix MCQ shuffle/multi-select + modal label crash src/utils/helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10,4 +10,14 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-module.exports = { generateId, capitalize, sleep };
+// Fisher-Yates shuffle (returns a NEW shuffled copy, does not mutate input)
+function shuffle(array) {
+  const arr = array.slice();
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
+module.exports = { generateId, capitalize, sleep, shuffle };
