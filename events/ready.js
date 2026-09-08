2a920ba9a30849be18bd4c4f769275986ed240f4
nguyenhuukhoinguyen082-crypto <nguyenhuukhoinguyen082@gmail.com>
2026-09-08 14:49:25 +0700

    Remove unneeded file events/ready.js

--- events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'ready',
-  once: true,
-  execute(client) {
-    console.log(`Logged in as ${client.user.tag} — serving ${client.guilds.cache.size} guild(s).`);
-  },
-};
