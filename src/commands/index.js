3cb959a5c2ec76b2fc0a3e2368b191f661f82235
nguyenhuukhoinguyen082-crypto <nguyenhuukhoinguyen082@gmail.com>
2026-09-08 14:29:55 +0700

    Update src/commands/index.js

---
+++ src/commands/index.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName('take-test')
+    .setDescription('Start the Phase 1 examination')
+    .toJSON(),
+
+  new SlashCommandBuilder()
+    .setName('host-eval')
+    .setDescription('Post a Phase 2 evaluation hosting announcement')
+    .addStringOption(opt =>
+      opt.setName('department').setDescription('Department').setRequired(true)
+        .addChoices(
+          { name: 'Flight Deck', value: 'Flight Deck' },
+          { name: 'Cabin Crew', value: 'Cabin Crew' },
+          { name: 'Ground Crew', value: 'Ground Crew' },
+        ))
+    .addStringOption(opt => opt.setName('airport').setDescription('Route/Airport').setRequired(true))
+    .addStringOption(opt => opt.setName('co-hosts').setDescription('Co-Hosts/Trainers'))
+    .addStringOption(opt => opt.setName('time').setDescription('Evaluation time').setRequired(true))
+    .toJSON(),
+
+  new SlashCommandBuilder()
+    .setName('join-eval')
+    .setDescription('Post a Phase 2 join message with a private server link')
+    .addStringOption(opt => opt.setName('server-link').setDescription('Private server link').setRequired(true))
+    .toJSON(),
+
+  new SlashCommandBuilder()
+    .setName('grade-phase2')
+    .setDescription('Start Phase 2 evaluation grading')
+    .toJSON(),
+
+  new SlashCommandBuilder()
+    .setName('request-supervision')
+    .setDescription('Request a Phase 3 live flight supervision')
+    .toJSON(),
+];
+
+module.exports = commands;
