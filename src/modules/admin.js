1770bd4ba96c095ca1447eb3fb9bd9a9500c2681
nguyenhuukhoinguyen082-crypto <nguyenhuukhoinguyen082@gmail.com>
2026-09-08 15:42:29 +0700

    Update spawn-exam-panel embed content src/modules/admin.js

--- src/modules/admin.js
+++ src/modules/admin.js
@@ -52,17 +52,32 @@ async function spawnExamPanel(interaction) {
 
   const embed = new EmbedBuilder()
     .setColor('#003D9C')
-    .setTitle('KLM Academy Phase 1 Examination')
+    .setTitle('Phase 1: Self-Study + Test')
     .setDescription(
-      'Welcome to the **KLM Academy Phase 1 Examination**! 🎓\n\n' +
-      'This examination tests your knowledge for your chosen department. ' +
-      'It consists of **6 Multiple Choice Questions** and **4 Written Questions**. ' +
-      'The test will be delivered to you via **Direct Message**.'
+      '-# Developed by vietnamtrainingpilot\n\n' +
+      '👋 Greetings all Trainees. Welcome to KLM Academy, to get a step closer to KLM Staff Team, you will go through a list of training.\n\n' +
+      'What you are looking here is **Phase 1.** As stated above the title, you are granted access to the ' +
+      '**training materials** that contains the majority of the studying information. You should read all of it ' +
+      'and memorize to use in the test and in your evaluation. Don\'t worry about asking questions, it is good to ask ' +
+      'questions on things that you do not understand. You can contact any Instructors for help!\n\n' +
+      'For the test, there will be **10 questions** (6 multiple choice questions + 4 written questions) about the ' +
+      'knowledge you\'ve studied from the handbook and some questions in there also have situations so from that knowledge ' +
+      'you will have to solve problems. More information can be viewed when you see the test. You should score at least ' +
+      '**80%** on the test to be able to pass the stage. All tests are taken on Google Forms, and results will be at ' +
+      `<#${config.channels.phase1Results}>.\n\n` +
+      `After passing, you will be moved on to <@&${config.roles.phase2}> to complete your second phase.\n\n` +
+      'Since you all have access to the materials, you are required to **NOT** leak of any these materials outside this ' +
+      'academy and if you violate that, you are blacklisted from being a staff member at KLM.'
     )
     .addFields(
-      { name: '📝 Format', value: 'Section 1: MCQ (12 points)\nSection 2: Written (12 points)\n**Total: 24 points**', inline: true },
-      { name: '✅ Pass Mark', value: `**${config.exam.passThreshold}/24** (75%)`, inline: true },
-      { name: '🔄 Attempts', value: 'You have limited attempts. Answer carefully!', inline: true },
+      {
+        name: '📚 Materials',
+        value: '> Select the materials you want to learn. Only open one that is your department. \n' +
+          '- [**Flight Deck Studying Guide**](https://canva.link/vi7qxzv7e9zpf2f)\n' +
+          '- [**Cabin Crew Studying Guide**](https://canva.link/xsgj8ujvem4hk0k)\n' +
+          '- [**Ground Crew Studying Guide**](https://canva.link/c99yggkr8nuv354)\n\n' +
+          'Good luck',
+      }
     )
     .setFooter({ text: 'KLM Academy - Make your dream come true at KLM Flight Academy' })
     .setThumbnail('https://cdn.discordapp.com/emojis/1543831346856853554.png');
@@ -76,7 +91,7 @@ async function spawnExamPanel(interaction) {
   );
 
   await interaction.reply({
-    content: 'Here is your Phase 1 Exam panel! Mark students who want to take the test by clicking the button below.',
+    content: 'Here is your Phase 1 Exam panel! Trainees can learn the materials and start the test by clicking the button below.',
     embeds: [embed],
     components: [buttonRow],
   });
